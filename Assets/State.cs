91bcebcb44e470275907e331ca4aeff9367017bf
Edward Martinez <edmartinez12389@gmail.com>
2020-06-23 00:10:48 -0400

    Serialized an array for next states, and created a public method for the same function.

--- Assets/State.cs
+++ Assets/State.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class State : ScriptableObject
 {
     [TextArea(14, 10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
 
     public string GetStateStory()
     {
         return storyText;
+    }
+    public State[] GetNextStates()
+    {
+        return nextStates;
     }
 }
 
